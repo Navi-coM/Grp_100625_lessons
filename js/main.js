1de68d3ac6097ef3f1c42390cbf335dbda81f85f
Navi-coM <navicom17@gmail.com>
2025-08-26 21:50:34 +0300

    Create new lesson 24 Basic JS

---
+++ js/main.js
@@ -0,0 +1,67 @@
+'use strict';
+
+let userName;
+
+userName = 'Ivan';
+
+// console.log(userName);
+// console.log(userName);
+
+
+// Назва змінної повина інтуітивно вказувати на те що всередині 
+// a-Z, 0-9, _, $
+
+// let frontEndTeacher = 'Ivan';
+// let front_end_teacher = 'Ivan';
+
+// let $;
+// let _ = 123;
+
+// console.log($);
+// console.log(userName);
+
+// ------------------ Варіанти створення змінних ----------
+
+// 1) var - застарілий варіант.
+var itSchool = 'beetroot';
+// var itSchool = 'beetroot';
+// var itSchool = 'beetroot';
+var it_school = 'beetroot';
+
+// var userName;
+// console.log(userName);
+// var userName;
+// console.log(userName);
+
+
+// 2) let - застарілий варіант. (ES2015)
+
+let stydent_2 = 'Serhij';
+stydent_2 = 'Anton';
+let stydent_3 = 'Marho';
+
+console.log(stydent_2 );
+
+
+// 3) const - застарілий варіант. (ES2015)
+
+const stydent_4 = 'Illa';
+
+// stydent_4 = 'Svitlana';
+
+const stydent_5 = 'Lesia';
+
+// stydent_4 = 'Oleh';
+
+const COLOR_RED = '#ccdd11';
+
+// ------------------------------------------
+
+let numb1 = 10;
+let numb2 = 5;
+let result = numb1 + numb2;
+
+console.log(result);
+
+
+
