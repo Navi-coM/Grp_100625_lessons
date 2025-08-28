041c2eafac01ced183b48f0f1f15f9bb221d748f
Navi-coM <navicom17@gmail.com>
2025-08-28 22:05:45 +0300

    Create new lesson 25 js type data

---
+++ js/default_function.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// 1. alert();
+// 2. confirm();
+// 3. prompt();
+
+// 1. alert
+
+let helloMessage = 'Hello my favorite group!';
+
+// alert(helloMessage);
+
+// 2. confirm();
+
+// let isYouProgrammer = confirm('Are you programmer?'); 
+
+// console.log(isYouProgrammer);
+
+// 3. prompt();
+
+// let userName = prompt('Enter your name','');
+
+// console.log(userName);
+
+let squareWidth = Number(prompt('Enter width square',''));
+console.log(typeof(squareWidth));
+console.log(squareWidth);
+// '8'
+
+let squarePerimetr = squareWidth * 4;
+
+console.log(squarePerimetr);
+
